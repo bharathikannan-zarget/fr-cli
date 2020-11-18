9fcef8111c81e814f7067ecd7b79d9845300a1da
Bharathi Kannan R <bharathi.kannan@freshworks.com>
2020-11-18 13:56:05 +0530

    Added requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
+install_requires = [
+    'tabulate>=0.8.7',
+    'fire>=0.3.1'
+]
+
 setuptools.setup(
 	name = "fr-cli",
 	version = "1.0.0",
